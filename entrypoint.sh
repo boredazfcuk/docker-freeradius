0db9f08527552e2889cd0eda7e7d7c6754c1b6c6
boredazfcuk <boredazfcuk@hotmail.co.uk>
2021-07-16 13:31:00 +0100

    Please check change.log

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,7 +21,7 @@ Initialise(){
    if [ "${secondarykdc_name}" ]; then echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Secondary KDC: ${secondarykdc_name}.${dns_name}"; fi
    echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Domain Computers SID: ${domain_computers_sid}"
    echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    VPN Users SID: ${vpn_users_sid}"
-   echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Starting Winbind: $(/etc/init.d.winbind start)"
+   echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Starting Winbind: $(/etc/init.d/winbind start)"
 }
 
 AddFilterStripUsername(){
