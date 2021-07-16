c0ca63e1719d41eec28586797690def7689d56bd
boredazfcuk <boredazfcuk@hotmail.co.uk>
2021-07-16 15:47:56 +0100

    Please check change.log

--- entrypoint.sh
+++ entrypoint.sh
@@ -54,59 +54,59 @@ AmendConfig(){
    local default_file
    default_file="${config_dir}/default"
    echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Amend default file: ${default_file}"
-   if [ "$(grep -c DOMAINNAME "${default_file}")" > 0 ]; then
+   if [ "$(grep -c DOMAINNAME "${default_file}")" -gt 0 ]; then
       sed -i "s/DOMAINNAME/${domain_name}/" "${default_file}"
    fi
-   if [ "$(grep -c TLDNAME "${default_file}")" > 0 ]; then
+   if [ "$(grep -c TLDNAME "${default_file}")" -gt 0 ]; then
       sed -i "s/TLDNAME/${tld_name}/" "${default_file}"
    fi
    local inner_tunnel_file
    inner_tunnel_file="${config_dir}/inner-tunnel"
    echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Amend inner-tunnel file: ${inner_tunnel_file}"
-   if [ "$(grep -c DOMAINNAME "${inner_tunnel_file}")" > 0 ]; then
+   if [ "$(grep -c DOMAINNAME "${inner_tunnel_file}")" -gt 0 ]; then
       sed -i "s/DOMAINNAME/${domain_name}/" "${inner_tunnel_file}"
    fi
-   if [ "$(grep -c TLDNAME "${inner_tunnel_file}")" > 0 ]; then
+   if [ "$(grep -c TLDNAME "${inner_tunnel_file}")" -gt 0 ]; then
       sed -i "s/TLDNAME/${tld_name}/" "${inner_tunnel_file}"
    fi
    local mschap_file
    mschap_file="${config_dir}/mschap"
    echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Amend mschap file: ${mschap_file}"
-   if [ "$(grep -c DOMAINNAME "${mschap_file}")" > 0 ]; then
+   if [ "$(grep -c DOMAINNAME "${mschap_file}")" -gt 0 ]; then
       sed -i "s/DOMAINNAME/${domain_name^^}/g" "${mschap_file}"
    fi
-   if [ "$(grep -c DOMAINCOMPUTERSSID "${mschap_file}")" > 0 ]; then
+   if [ "$(grep -c DOMAINCOMPUTERSSID "${mschap_file}")" -gt 0 ]; then
       sed -i "s/DOMAINCOMPUTERSSID/${domain_computers_sid}/" "${mschap_file}"
    fi
-   if [ "$(grep -c VPNUSERSSID "${mschap_file}")" > 0 ]; then
+   if [ "$(grep -c VPNUSERSSID "${mschap_file}")" -gt 0 ]; then
       sed -i "s/VPNUSERSSID/${vpn_users_sid}/" "${mschap_file}"
    fi
    echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Amend ntlm_auth file"
    local ntlm_auth_file
    ntlm_auth_file="${config_dir}/ntlm_auth"
-   if [ "$(grep -c DOMAINNAME "${ntlm_auth_file}")" > 0 ]; then
+   if [ "$(grep -c DOMAINNAME "${ntlm_auth_file}")" -gt 0 ]; then
       sed -i "s/DOMAINNAME/${domain_name^^}/" "${ntlm_auth_file}"
    fi
    local krb5_file
    krb5_file="${config_dir}/krb5.conf"
    echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Amend krb5 file: ${krb5_file}"
-   if [ "$(grep -c LCDOMAINNAME "${krb5_file}")" > 0 ]; then
+   if [ "$(grep -c LCDOMAINNAME "${krb5_file}")" -gt 0 ]; then
       sed -i "s/LCDOMAINNAME/${domain_name,,}/" "${krb5_file}"
    fi
-   if [ "$(grep -c DOMAINNAME "${krb5_file}")" > 0 ]; then
+   if [ "$(grep -c DOMAINNAME "${krb5_file}")" -gt 0 ]; then
       sed -i "s/DOMAINNAME/${domain_name^^}/" "${krb5_file}"
    fi
-   if [ "$(grep -c LCTLDNAME "${krb5_file}")" > 0 ]; then
+   if [ "$(grep -c LCTLDNAME "${krb5_file}")" -gt 0 ]; then
       sed -i "s/LCTLDNAME/${tld_name,,}/" "${krb5_file}"
    fi
-   if [ "$(grep -c TLDNAME "${krb5_file}")" > 0 ]; then
+   if [ "$(grep -c TLDNAME "${krb5_file}")" -gt 0 ]; then
       sed -i "s/TLDNAME/${tld_name^^}/" "${krb5_file}"
    fi
-   if [ "$(grep -c PRIMARYKDC "${krb5_file}")" > 0 ]; then
+   if [ "$(grep -c PRIMARYKDC "${krb5_file}")" -gt 0 ]; then
       sed -i "s/PRIMARYKDC/${primarykdc_name^^}/" "${krb5_file}"
    fi
    if [ "${secondarykdc_name}" ]; then
-      if [ "$(grep -c SECONDARYKDC "${krb5_file}")" > 0 ]; then
+      if [ "$(grep -c SECONDARYKDC "${krb5_file}")" -gt 0 ]; then
          sed -i "s/SECONDARYKDC/${secondarykdc_name^^}/" "${krb5_file}"
       fi
    else
@@ -115,23 +115,23 @@ AmendConfig(){
    local smb_file
    smb_file="${config_dir}/smb.conf"
    echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Amend smb file: ${smb_file}"
-   if [ "$(grep -c LCDOMAINNAME "${smb_file}")" > 0 ]; then
+   if [ "$(grep -c LCDOMAINNAME "${smb_file}")" -gt 0 ]; then
       sed -i "s/LCDOMAINNAME/${domain_name,,}/" "${smb_file}"
    fi
-   if [ "$(grep -c DOMAINNAME "${smb_file}")" > 0 ]; then
+   if [ "$(grep -c DOMAINNAME "${smb_file}")" -gt 0 ]; then
       sed -i "s/DOMAINNAME/${domain_name^^}/g" "${smb_file}"
    fi
-   if [ "$(grep -c LCTLDNAME "${smb_file}")" > 0 ]; then
+   if [ "$(grep -c LCTLDNAME "${smb_file}")" -gt 0 ]; then
       sed -i "s/LCTLDNAME/${tld_name,,}/" "${smb_file}"
    fi
-   if [ "$(grep -c PRIMARYKDC "${smb_file}")" > 0 ]; then
+   if [ "$(grep -c PRIMARYKDC "${smb_file}")" -gt 0 ]; then
       sed -i "s/PRIMARYKDC/${primarykdc_name^^}/" "${smb_file}"
    fi
-   if [ "$(grep -c KRB5CONFIGDIR "${smb_file}")" > 0 ]; then
+   if [ "$(grep -c KRB5CONFIGDIR "${smb_file}")" -gt 0 ]; then
       sed -i 's/KRB5CONFIGDIR/${config_dir}/' "${smb_file}"
    fi
    local clients_file
-   clients_file="${config_dir]/clients.conf"
+   clients_file="${config_dir}/clients.conf"
    echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Amend clients file: ${clients_file}"
    if [ "$(tail -1 "${clients_file}")" != "\$INCLUDE ${config_dir}/custom_clients.conf" ]; then
       echo "\$INCLUDE ${config_dir}/custom_clients.conf" >> "${clients_file}"
@@ -139,7 +139,7 @@ AmendConfig(){
 }
 
 TestDomainJoin(){
-   echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Domain member status: $(echo ${join_status})"
+   echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Domain member status: ${join_status}"
    if [ "${join_status}" != "Join is OK" ]; then
       echo "$(date '+%Y-%m-%d %H:%M:%S') ERROR:   ***** Container ${samba_name} is not joined to the domain *****"
       echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    To join the domain, connect to the container with: docker exec -it <container_name> /bin/bash"
